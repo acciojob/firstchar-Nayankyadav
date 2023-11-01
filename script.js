df6c5d38712d444153cc1b36a6a2155468125dff
Nayankyadav <144725447+Nayankyadav@users.noreply.github.com>
2023-11-01 17:26:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstChar(text) {
   // your code here
+	function firstChar(inputString) {
+    for (let i = 0; i < inputString.length; i++) {
+        if (inputString[i] !== ' ') {
+            return inputString[i];
+        }
+    }
+    return '';
+}
+
+// Example usages
+console.log(firstChar(' Rosa Parks '));    // Output: 'R'
+console.log(firstChar(' Hello World '));   // Output: 'H'
+console.log(firstChar(' '));               // Output: ''
+console.log(firstChar(''));                // Output: ''
+
 }
 
 // Do not change the code below
